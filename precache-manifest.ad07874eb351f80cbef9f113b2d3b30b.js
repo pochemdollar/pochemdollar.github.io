ab9dfeeac0fc35724538b4c2c2e49c27323aa3fe
Ruslan Arshinnik <rarshinnik@justcoded.co>
2018-12-23 02:24:26 +0200

    Updates

---
+++ precache-manifest.ad07874eb351f80cbef9f113b2d3b30b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f162241307e9200f759",
+    "url": "/static/js/main.7f162241.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7f162241307e9200f759",
+    "url": "/static/css/main.a4a15bc5.chunk.css"
+  },
+  {
+    "revision": "f9282ffdd27b9120e9db31e991226fd6",
+    "url": "/index.html"
+  }
+];
