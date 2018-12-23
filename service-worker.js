a7ba838414ab95d97fed2ecd1042694318d386d5
Ruslan Arshinnik <rarshinnik@justcoded.co>
2018-12-23 02:43:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pochemdollar.github.io/precache-manifest.3f865b4ac6ac96eef0ad25ea01b5511c.js"
+  "/precache-manifest.ad07874eb351f80cbef9f113b2d3b30b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pochemdollar.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
